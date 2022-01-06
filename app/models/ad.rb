b32fbd97d79cb1a177eba1cf4f73b5d1ae9ec210
Thiago Giammattey <giammattey@gmail.com>
2022-01-06 10:14:32 -0300

    use to_param to make SEO-friendly URLs

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -3,5 +3,9 @@ class Ad < ApplicationRecord
   validates :title, presence: true
   validates :body, length: { minimum: 10 }
   validates :studio, numericality: { only_integer: true }
+
+  def to_param
+    id.to_s + "-" + title.parameterize
+  end
   
 end
