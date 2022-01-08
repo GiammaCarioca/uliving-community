a55b8195ac4c0d653b88650a3b12515dc669950c
Thiago Giammattey <giammattey@gmail.com>
2022-01-08 11:50:13 -0300

    add comments

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,5 +1,8 @@
 class Ad < ApplicationRecord
 
+  # add an association that has a 1-to-many relationship
+  has_many :comments
+
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
   validates :studio, numericality: { only_integer: true }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_08_134909) do
+ActiveRecord::Schema.define(version: 2022_01_08_144511) do
 
   create_table "ads", force: :cascade do |t|
     t.string "title"
@@ -22,4 +22,13 @@ ActiveRecord::Schema.define(version: 2022_01_08_134909) do
     t.string "category"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "ad_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ad_id"], name: "index_comments_on_ad_id"
+  end
+
+  add_foreign_key "comments", "ads"
 end
