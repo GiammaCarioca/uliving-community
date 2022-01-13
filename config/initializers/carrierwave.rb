014bb7277585274cc3b7d38b65e7727fb959531f
Thiago Giammattey <giammattey@gmail.com>
2022-01-13 12:43:07 -0300

    trying to fix credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,5 +6,5 @@ CarrierWave.configure do |config|
     aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],   
     region:                'sa-east-1',                  
   }
-  config.fog_directory  = 'uliving-community'         
+  config.fog_directory  = Rails.application.credentials.aws[:bucket]        
 end
