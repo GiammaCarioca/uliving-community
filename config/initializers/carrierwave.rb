2448e1a109d994a6a8bb4bb630f077d48eaf3582
Thiago Giammattey <giammattey@gmail.com>
2022-01-13 13:11:26 -0300

    trying to fix credentials on production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,9 +2,9 @@ CarrierWave.configure do |config|
   config.fog_provider = "fog/aws"
   config.fog_credentials = {
     provider:              'AWS',
-    aws_access_key_id:     Rails.application.credentials.aws[:access_key_id],   
-    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],   
+    aws_access_key_id:     Rails.application.credentials.aws[:s3_access_key_id],   
+    aws_secret_access_key: Rails.application.credentials.aws[:s3_secret_access_key],   
     region:                'sa-east-1',                  
   }
-  config.fog_directory  = Rails.application.credentials.aws[:bucket]        
+  config.fog_directory  = Rails.application.credentials.aws[:s3_bucket_name]        
 end
