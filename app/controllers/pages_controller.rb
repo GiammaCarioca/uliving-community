7d5bdb8ea376267da14fad3a73dfde51183838fb
Thiago Giammattey <giammattey@gmail.com>
2022-01-14 16:54:48 -0300

    trying to fix something

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
     @content = Page.find_by(url: "home")
 
-    @latest_ads = Ad.where("created_at > ?", 1.week.ago.today?)
+    @latest_ads = Ad.where("created_at > ?", 1.week.ago)
 
     @featured_ads = Ad.where(is_featured: true)
   end
