bd0fb5bf40379381163cc5fda549aecf707f3314
Thiago Giammattey <giammattey@gmail.com>
2022-01-14 12:24:02 -0300

    add content pages

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   resource :account
 
-  root "ads#index"
+  get "about", to: "pages#about"
 
+  root "ads#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_111700) do
+ActiveRecord::Schema.define(version: 2022_01_14_150239) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -51,6 +51,15 @@ ActiveRecord::Schema.define(version: 2022_01_14_111700) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.index ["ad_id"], name: "index_comments_on_ad_id"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "url"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
