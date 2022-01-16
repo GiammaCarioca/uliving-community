49dc1b172cba0f8883a3b7ac6b3181440dabe827
Thiago Giammattey <giammattey@gmail.com>
2022-01-16 18:59:21 -0300

    customizing Active Admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,22 +1,26 @@
 ActiveAdmin.register User do
+  permit_params :real_name, :username, :email, :password_confirmation, :photo, :studio_number, :phone_number, :is_admin, :birthday_date, :description
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  permit_params :real_name, :username, :email, :password_confirmation, :photo, :studio_number, :phone_number, :is_admin
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:real_name, :username, :email, :password_digest, :photo, :studio_number, :phone_number, :is_admin]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  form do |f|
+    f.inputs "User info" do
+      f.input :real_name
+      f.input :studio_number
+      f.input :phone_number
+      f.input :birthday_date
+      f.input :description
+    end
+
+    f.inputs "Account info" do
+      f.input :username
+      f.input :email
+      f.input :password_confirmation
+      f.input :is_admin
+    end
+
+    f.actions
+  end
 
   controller do
     resources_configuration[:self][:finder] = :find_by_username
   end
-  
 end
